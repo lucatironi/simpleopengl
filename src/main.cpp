c91c95a44fcf15491b70c7267e98571932066378
Luca Tironi <luca.tironi@gmail.com>
2018-03-28 22:15:44 +0200

    Change window title

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "SimpleOpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
