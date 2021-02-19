14f039d13c704139a69b10d0fa169ff5748c6831
Luca Tironi <luca.tironi@gmail.com>
2021-02-19 22:39:35 +0100

    Typo

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 static void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
 static void ProcessInput(GLFWwindow* window);
 
-const char *vertexShaderSource = "#version 330 core\n"
+const char* vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
                                  "layout (location = 1) in vec3 aColor;\n"
                                  "out vec3 ourColor;\n"
@@ -17,7 +17,7 @@ const char *vertexShaderSource = "#version 330 core\n"
                                  "   ourColor = aColor;\n"
                                  "}\0";
 
-const char *fragmentShaderSource = "#version 330 core\n"
+const char* fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
                                    "in vec3 ourColor;\n"
                                    "void main()\n"
